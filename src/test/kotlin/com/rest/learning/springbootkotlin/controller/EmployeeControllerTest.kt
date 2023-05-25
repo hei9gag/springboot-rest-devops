fd2acc2370aea332cfb380ce032d1b828137154f
Chung, Brian <brian.chung@zuhlke.com>
2023-05-25 11:55:56 +0800

    remove unused import

--- src/test/kotlin/com/rest/learning/springbootkotlin/controller/EmployeeControllerTest.kt
+++ src/test/kotlin/com/rest/learning/springbootkotlin/controller/EmployeeControllerTest.kt
@@ -1,6 +1,5 @@
 package com.rest.learning.springbootkotlin.controller
 
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -8,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @AutoConfigureMockMvc
